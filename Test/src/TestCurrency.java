44abe3bb6ba57f6a90e511a8d55c095a64ca4fec
SuncheolKim <tinymin84@gmail.com>
2016-07-15 12:42:19 +0900

    First JUnit Commit

---
+++ Test/src/TestCurrency.java
@@ -0,0 +1,48 @@
+import com.tinymin.Dollar;
+import com.tinymin.Franc;
+import com.tinymin.Money;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tinymin on 16. 7. 13..
+ */
+public class TestCurrency
+{
+    @Test
+    public void testMulitipication() throws Exception
+    {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancMulitipication() throws Exception
+    {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() throws Exception
+    {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+
+        assertFalse(new Franc(5).equals(new Dollar(5)));
+    }
+
+    @Test
+    public void testCurrentcy() throws Exception
+    {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+}
