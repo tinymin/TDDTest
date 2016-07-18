3e617dd6d52682fb1163c2cfe809e178cf034d34
SuncheolKim <tinymin84@gmail.com>
2016-07-18 13:08:54 +0900

    1. 덧셈을 위해 테스트 추가 2. Money.plus() 메소드 추가

--- Test/src/TestCurrency.java
+++ Test/src/TestCurrency.java
@@ -38,4 +38,11 @@ public class TestCurrency
         assertEquals("USD", Money.dollar(1).getCurrentcy());
         assertEquals("CHF", Money.franc(1).getCurrentcy());
     }
+
+    @Test
+    public void testSimpleAddition() throws Exception
+    {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
--- src/main/java/com/tinymin/Money.java
+++ src/main/java/com/tinymin/Money.java
@@ -45,4 +45,9 @@ public class Money
     {
         return amount + ", " + currency;
     }
+
+    public Money plus(Money addend)
+    {
+        return new Money(amount + addend.amount, currency);
+    }
 }
